94c4b8489c61e8cff8338c7d123dd0c669216b4b
sha ke <82579992+sha-ce@users.noreply.github.com>
2021-06-18 20:33:33 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -43,9 +43,9 @@ const block = "block";
 const blocktext = "block-text";
 const namae = "name";
 const text = "text";
-let HTMLtext = "";
 
-function JSONGetTest() {
+
+function getThread() {
     fetch('https://t9f823.deta.dev/api/v1/threads')
         .then(response => {
             return response.json();
@@ -56,6 +56,28 @@ function JSONGetTest() {
             let length = data.length;
             for (let i = length - 1; i >= 0; i--) {
                 HTMLtext += "<div class=" + block + ">\n" + "<div class=" + blocktext + ">\n" + "<div class=" + namae + "><h5>" + data[i]["author"]["name"] + "</h5></div>\n" + "<div class=" + text + ">\n" + "<p>" + data[i]["name"] + "</p>\n" + "</div>\n</div>\n</div>";;
+            }
+            document.getElementById('threadscontainer').innerHTML = HTMLtext;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+let threadKey = 'xx8syv6jiea8';
+
+window.onload = function getContent() {
+    let HTMLtext = "";
+    fetch('https://t9f823.deta.dev/api/v1/threads/' + threadKey + '/posts?limit=50&page=1')
+        .then(response => {
+            return response.json();
+        })
+        .then(res => {
+            console.log(res);
+            const data = res;
+            let length = data.length;
+            for (let i = length - 1; i >= 0; i--) {
+                HTMLtext += "<div class=" + block + ">\n" + "<div class=" + blocktext + ">\n" + "<div class=" + namae + "><h5>" + data[i]["author"]["name"] + "</h5></div>\n" + "<div class=" + text + ">\n" + "<p>" + data[i]["content"] + "</p>\n" + "</div>\n</div>\n</div>";;
             }
             document.getElementById('threadscontainer').innerHTML = HTMLtext;
         })
